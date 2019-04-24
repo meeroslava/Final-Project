1d207d392ccac536acb6e9c08d7d39c07df75596
meeroslava <sapphirenymph@gmail.com>
2019-04-24 18:34:17 +0300

    updated DB

--- DB/DB.sql
+++ DB/DB.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.2.1 on Mon Apr 22 19:42:06 2019
+-- File generated with SQLiteStudio v3.2.1 on Wed Apr 24 18:33:56 2019
 --
 -- Text encoding used: System
 --
@@ -7,7 +7,24 @@ PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: Events
-CREATE TABLE Events (eventID INT PRIMARY KEY NOT NULL, openDate DATETIME, resolveDate DATETIME, description TEXT, duration DATETIME, tags VARCHAR (40) REFERENCES Tags (name), createdBy REFERENCES Users (Email), jira TEXT, related REFERENCES Events (eventID));
+CREATE TABLE Events (eventID INTEGER PRIMARY KEY AUTOINCREMENT, openDate DATETIME, resolveDate DATETIME, description TEXT, duration DATETIME, tags VARCHAR (40) REFERENCES Tags (name), createdBy REFERENCES Users (email), jira TEXT, related REFERENCES Events (eventID), subject TEXT);
+INSERT INTO Events (eventID, openDate, resolveDate, description, duration, tags, createdBy, jira, related, subject) VALUES (1, '2019-04-22 17:28:01', NULL, 'test', NULL, NULL, 'test@oht.com', NULL, NULL, NULL);
+INSERT INTO Events (eventID, openDate, resolveDate, description, duration, tags, createdBy, jira, related, subject) VALUES (2, '2019-04-22 17:28:01', NULL, 'test2', NULL, NULL, 'test@oht.com', NULL, NULL, NULL);
+INSERT INTO Events (eventID, openDate, resolveDate, description, duration, tags, createdBy, jira, related, subject) VALUES (3, '2019-04-22 17:28:01', NULL, 'test3', NULL, NULL, 'test@oht.com', NULL, NULL, NULL);
+INSERT INTO Events (eventID, openDate, resolveDate, description, duration, tags, createdBy, jira, related, subject) VALUES (4, '2019-04-22 17:28:01', NULL, 'as', NULL, NULL, 'test@oht.com', '2', 3, '1');
+
+-- Table: sqlitestudio_temp_table
+CREATE TABLE sqlitestudio_temp_table(
+  eventID INT,
+  openDate NUM,
+  resolveDate NUM,
+  description TEXT,
+  duration NUM,
+  tags TEXT,
+  createdBy,
+  jira TEXT,
+  related
+);
 
 -- Table: Tags
 CREATE TABLE Tags (name VARCHAR (40) PRIMARY KEY);
@@ -17,6 +34,7 @@ CREATE TABLE Updates (updateDate PRIMARY KEY, eventId REFERENCES Events (eventID
 
 -- Table: Users
 CREATE TABLE Users (email TEXT (40) PRIMARY KEY NOT NULL CHECK (Email LIKE '%___@___%'), name TEXT (40), role TIME (40));
+INSERT INTO Users (email, name, role) VALUES ('test@oht.com', 'tester', 'user');
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
--- html/addevent.php
+++ html/addevent.php
@@ -1,14 +1,29 @@
 <?php
-$db = new PDO('sqlite:mainDB.sqlite');
+/*$db = new PDO('sqlite:mainDB.sqlite');*/
+$db = new SQLite3('MainDB.db');
 
 $subject = $_POST['subject'];
 $jira = $_POST['jira'];
 $related = $_POST['related'];
-$description = $_POST['description'];
-
+$description = 'test';
+$createdBy= 'test@oht.com';
+$date = date("Y-m-d H:i:s");
 
 echo $subject;
 echo $jira;
 echo $related;
 echo $description;
+echo $createdBy;
+echo $date;
+
+$query = "INSERT INTO Events (subject, jira, related, description, createdBy, openDate) VALUES (1, 2, 3, 'as', 'test@oht.com' , '2019-04-22 17:28:01')"
+$query -> exec();
+/*
+$query = $db->prepare(
+    "INSERT INTO Events (subject, jira, related, description, createdBy, openDate) VALUES (1, 2, 3, 'as', 'test@oht.com' , '2019-04-22 17:28:01')");
+    $query->exec();
+
+
+$query->exec(array($subject, $jira, $related, $description, $createdBy, $date)); */
 ?>
+
